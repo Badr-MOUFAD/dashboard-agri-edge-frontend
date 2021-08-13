38117c3df91b67ce17aee5edb2c1bc7946d088b9
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2021-08-13 16:20:59 +0100

    general layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import PreviewComponent from "./components/Preview";
+import FooterComponent from "./components/Footer";
 
 export default function App() {
-  return <PreviewComponent />;
+  return (
+    <main class="bg-gray-100 dark:bg-gray rounded-2xl h-screen overflow-hidden relative">
+      <div class="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
+        {/* Header */}
+        {/* <header class="w-full items-center h-16 rounded-2xl z-40">
+          <span className="">This a header</span>
+        </header> */}
+
+        {/* content general view */}
+        <section className="grid grid-cols-12 gap-4">
+          {/* map container */}
+          <div class="col-start-1 col-end-7 w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
+            1
+          </div>
+          {/* graphs centroids container*/}
+          <div class="col-span-6 w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
+            2
+          </div>
+        </section>
+
+        {/* content region details */}
+        <section className="grid grid-cols-12 gap-4">
+          <div className="col-span-12 w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
+            content
+          </div>
+        </section>
+
+        {/* footer */}
+        <FooterComponent />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function (props) {
+  return (
+    <section className="mx-2">
+      <hr className="mt-8 mb-2" />
+
+      <div className="grid-col-12 flex justify-between">
+        <div className="col-span-2">
+          <span className="text-sm font-mono">Internship</span>
+        </div>
+        <div className="col-span-1">
+          <span className="text-sm font-mono">GitHub</span>
+        </div>
+      </div>
+    </section>
+  );
+}
