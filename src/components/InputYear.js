e8989114df197210b8fe51a33adac44b777030ee
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2021-08-18 01:34:49 +0100

    input with validation

--- src/components/InputYear.js
+++ src/components/InputYear.js
@@ -8,14 +8,14 @@ export function InputYears(props) {
   const { enteredYearsHandler } = props;
 
   return (
-    <div class="flex relative w-96">
-      <span class="rounded-l-md inline-flex items-center px-3 border-t bg-gray-100 border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+    <div className="flex relative w-96 overflow-visible z-40">
+      <span className="rounded-l-md inline-flex items-center px-3 border-t bg-gray-100 border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
         Years to include
       </span>
       <input
         type="text"
         id="input-years"
-        className="rounded-r-full flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-grey-600 focus:border-transparent"
+        className="rounded-r-full flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-grey-600 focus:border-transparent ring-red-500 ring-2"
         placeholder="Enter years separated by ,"
         value={yearsSeperatedComma}
         onChange={({ target }) => setYearsSeperatedComma(target.value)}
@@ -40,14 +40,16 @@ export function InputYears(props) {
         }}
       />
 
-      <ErrorComponent error={error} />
+      <p className="flex absolute text-xs -bottom-4 text-red-500">
+        Email is invalid
+      </p>
     </div>
   );
 }
 
 export function ErrorComponent(props) {
   const { error } = props;
-  if (true)
+  if (error) {
     return (
       <React.Fragment>
         <ErrorComponent size={16} color="red-500" />
@@ -56,5 +58,7 @@ export function ErrorComponent(props) {
         </p>
       </React.Fragment>
     );
-  else return <div></div>;
+  } else {
+    return <div></div>;
+  }
 }
